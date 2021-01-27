de4bfe0926ae369e87314441b3b1b431bf5ba8b5
yb00 <yagobraz98@gmail.com>
2021-01-26 22:13:46 -0300

    Add musictable body

--- src/components/MusicSelectTable.js
+++ src/components/MusicSelectTable.js
@@ -1,19 +1,30 @@
-import React from "react";
-
+import React, { useState } from "react";
 import { AgGridColumn, AgGridReact } from "ag-grid-react";
 
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-alpine.css";
 
+import "./MusicSelectTable.css";
+
 const onFirstDataRendered = (params) => {
   params.api.sizeColumnsToFit();
 };
 
-const MusicSelectTable = () => {
+const MusicSelectTable = ({ src }) => {
   return (
     <div className="ag-theme-alpine select-table">
-      <AgGridReact rowData={bgm} onFirstDataRendered={onFirstDataRendered}>
-        <AgGridColumn field="metadata.title"></AgGridColumn>
+      <AgGridReact
+        rowData={src}
+        onFirstDataRendered={onFirstDataRendered}
+        pagination={true}
+        paginationAutoPageSize={true}
+      >
+        <AgGridColumn headerName="Filename" field="filename"></AgGridColumn>
+        <AgGridColumn headerName="Youtube ID" field="youtube"></AgGridColumn>
+        <AgGridColumn
+          headerName="Metadata"
+          field="metadata.title"
+        ></AgGridColumn>
       </AgGridReact>
     </div>
   );
