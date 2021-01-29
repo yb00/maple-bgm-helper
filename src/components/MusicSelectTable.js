0b1e785d12ded5abdc897d82fcef563b2fbf6a41
yb00 <yagobraz98@gmail.com>
2021-01-28 22:57:15 -0300

    Add id on click

--- src/components/MusicSelectTable.js
+++ src/components/MusicSelectTable.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { AgGridColumn, AgGridReact } from "ag-grid-react";
 
 import "ag-grid-community/dist/styles/ag-grid.css";
@@ -11,19 +11,44 @@ const onFirstDataRendered = (params) => {
 };
 
 const MusicSelectTable = ({ src }) => {
+  const [selection, setSelection] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(
+      src.map((obj) => {
+        return { ...obj, selected: 0 };
+      })
+    );
+  }, [src]);
+
+  const addLink = (obj) => {
+    const id = obj.youtube
+    if (selection.indexOf(id) === -1) {
+      setSelection(selection.push(id));
+      console.log(data)
+    }
+  };
+
   return (
     <div className="ag-theme-alpine select-table">
       <AgGridReact
-        rowData={src}
+        rowData={data}
         onFirstDataRendered={onFirstDataRendered}
         pagination={true}
         paginationAutoPageSize={true}
+        rowClassRules={{ "selected-cell": "data.selected === 1" }}
       >
         <AgGridColumn headerName="Filename" field="filename"></AgGridColumn>
-        <AgGridColumn headerName="Youtube ID" field="youtube"></AgGridColumn>
+        <AgGridColumn
+          headerName="Youtube ID"
+          field="youtube"
+          onCellClicked={(params) => {addLink(params.data)}}
+        ></AgGridColumn>
         <AgGridColumn
           headerName="Metadata"
           field="metadata.title"
+          cellClassRules
         ></AgGridColumn>
       </AgGridReact>
     </div>
