b12146a2521f2fee9f0a43aed221b08ca234a7c9
yb00 <yagobraz98@gmail.com>
2021-07-15 23:28:28 -0300

    Fix tsconfigs

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.tsx",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "index_bundle.js",
