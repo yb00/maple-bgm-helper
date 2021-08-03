58aed8f1f9b146e66d9763bd4938075e6209fdde
yb00 <yagobraz98@gmail.com>
2021-08-02 23:13:25 -0300

    Format

--- src/components/MusicSelectTable.tsx
+++ src/components/MusicSelectTable.tsx
@@ -1,19 +1,22 @@
-import React, { useRef } from 'react'
-import { AgGridReact } from 'ag-grid-react'
+import React, { useRef } from 'react';
+import { AgGridReact } from 'ag-grid-react';
 import {
     ColDef,
     ColumnApi,
     FirstDataRenderedEvent,
+    SelectionChangedEvent,
     GridApi,
     GridOptions,
     GridReadyEvent,
-} from 'ag-grid-community'
+} from 'ag-grid-community';
 
-import 'ag-grid-community/dist/styles/ag-grid.css'
-import 'ag-grid-community/dist/styles/ag-theme-alpine.css'
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
-import './MusicSelectTable.css'
-import { useDataSourceState } from '../context/DataSourceContext'
+import './MusicSelectTable.css';
+import { useDataSourceState } from '../context/DataSourceContext';
+import { usePlaylistState } from '../context/PlaylistContext';
+import { IMusicRecordJson } from '../models/DataModel';
 
 const getGridOptions: () => GridOptions = () => {
     return {
@@ -29,8 +32,8 @@ const getGridOptions: () => GridOptions = () => {
             resizable: true,
         },
         domLayout: 'autoHeight',
-    }
-}
+    };
+};
 
 const getColDef: () => ColDef[] = () => {
     return [
@@ -39,35 +42,46 @@ const getColDef: () => ColDef[] = () => {
             field: 'metadata.title',
             checkboxSelection: true,
             cellRenderer: (params) => {
-                const id = params.data.youtube
-                const title = params.data.metadata.title
+                const id = params.data.youtube;
+                const title = params.data.metadata.title;
                 return (
                     '<a href="https://youtu.be/' +
                     id +
                     '" target="_blank" rel="noopener noreferrer">#</a>' +
                     ` ${title}`
-                )
+                );
             },
         },
         {
             headerName: 'Year',
             field: 'metadata.year',
         },
-    ]
-}
+    ];
+};
 
 const MusicSelectTable: React.FC<{ query: string | undefined }> = ({
     query,
 }) => {
+    const dataSource = useDataSourceState();
     const playlist = usePlaylistState();
+    const gridApi = useRef<GridApi | null>(null);
+    const gridColumnApi = useRef<ColumnApi | null>(null);
+    const colDef = useRef<ColDef[]>([]);
+    const gridOptions = useRef<GridOptions | undefined>(undefined);
+    colDef.current = getColDef();
+    gridOptions.current = getGridOptions();
 
     const onGridReady = (params: GridReadyEvent): void => {
-        gridApi.current = params.api
-        gridColumnApi.current = params.columnApi
-    }
+        gridApi.current = params.api;
+        gridColumnApi.current = params.columnApi;
+    };
     const onFirstDataRendered = (event: FirstDataRenderedEvent): void => {
-        event.columnApi.autoSizeAllColumns()
-    }
+        event.columnApi.autoSizeAllColumns();
+    };
+    const onSelectionChanged = (event: SelectionChangedEvent): void => {
+        // var nodes = event.api.getSelectedNodes();
+    };
+
     return (
         <div className="ag-theme-alpine music-table-wrapper">
             <input type="text" className="table-search-input"></input>
@@ -86,9 +100,10 @@ const MusicSelectTable: React.FC<{ query: string | undefined }> = ({
                 rowClassRules={{ 'row-selected': 'data.selected === true' }}
                 rowSelection="multiple"
                 rowMultiSelectWithClick={true}
+                onSelectionChanged={onSelectionChanged}
             ></AgGridReact>
         </div>
-    )
-}
+    );
+};
 
-export default MusicSelectTable
+export default MusicSelectTable;
